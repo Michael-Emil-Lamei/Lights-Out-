79e3786e96773af7e4c2078d73ca79a7226ae64d
Michael Emil Lamei Fawzy <Michaellemill2009@gmail.com>
2026-08-16 15:44:09 +0300

    JS constants
    Defined main java script constants, and renamed some variables in the html file because it was confusing with the java script constant names.

---
+++ script.js
@@ -0,0 +1,6 @@
+const reactButton = document.querySelector("#reaction-button");
+const lights = document.querySelectorAll(".light");
+const resultsDisplay = document.querySelector("#results");
+
+console.log(reactButton, lights, resultsDisplay);
+
